e7d426ae891ecfcbb6ab6e19e4b0dcd5abbee81e
MachinAdam <adam.aga74@gmail.com>
2019-07-07 12:21:34 +0200

    Add file

---
+++ home.js
@@ -0,0 +1,7 @@
+const history = document.getElementById('history')
+const composition = document.getElementById('composition')
+const theBestHits = document.getElementById('theBestHits')
+
+history.addEventListener('click', function(){
+    
+})
