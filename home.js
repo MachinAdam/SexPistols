8203e09677ec1813ccbb48ec1263fad92ef1a35e
MachinAdam <adam.aga74@gmail.com>
2019-07-15 21:19:29 +0200

    Add file

--- home.js
+++ home.js
@@ -2,6 +2,10 @@ const history = document.getElementById('history')
 const composition = document.getElementById('composition')
 const theBestHits = document.getElementById('theBestHits')
 
-history.addEventListener('click', function(){
-    
-})
+const header = document.createElement('header')
+document.body.appendChild(header)
+
+const title = "Sex Pistols Początki"
+
+header.id = "NewTitle"
+header.innerHTML = title;
